cc4094c00ee4e65935c9ac69aaf67b8f4c031f2f
Patrick Tavares <tavarespatrick01@gmail.com>
2020-05-27 14:15:00 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# zsh-sdkman
+
+zsh plugin for installing and loading [sdkman](https://github.com/sdkman)
+
+This plugin come with completion scripts and aliases
+
+## Table of content
+
+- [zsh-sdkman](#zsh-sdkman)
+  - [Usage](#usage)
+  - [Updating sdkman](#updating-sdkman)
+  - [License](#license)
+
+## Dependencies
+
+As this plugin install [sdkman](https://github.com/sdkman), you'll need some packages to ensure this plugin to work :
+
+- `bash`
+- `zip`
+- `unzip`
+- `curl`
+
+## Usage
+
+Once the plugin installed, `sdkman` will be available
+
+- Using [Antigen](https://github.com/zsh-users/antigen)
+
+Bundle `zsh-sdkman` in your `.zshrc`
+
+```shell
+antigen bundle ptavares/zsh-sdkman
+```
+
+- Using [zplug](https://github.com/b4b4r07/zplug)
+
+Load `zsh-sdkman` as a plugin in your `.zshrc`
+
+```shell
+zplug "ptavares/zsh-sdkman"
+```
+
+- Using [zgen](https://github.com/tarjoilija/zgen)
+
+Include the load command in your `.zshrc`
+
+```shell
+zget load ptavares/zsh-sdkman
+```
+
+- As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
+
+Clone `zsh-sdkman` into your custom plugins repo and load as a plugin in your `.zshrc`
+
+```shell
+git clone https://github.com/ptavares/zsh-sdkman.git ~/.oh-my-zsh/custom/plugins/zsh-sdkman
+```
+
+```shell
+plugins+=(zsh-sdkman)
+```
+
+Keep in mind that plugins need to be added before `oh-my-zsh.sh` is sourced.
+
+- Manually
+
+Clone this repository somewhere (`~/.zsh-sdkman` for example) and source it in your `.zshrc`
+
+```shell
+git clone https://github.com/ptavares/zsh-sdkman ~/.zsh-sdkman
+```
+
+```shell
+source ~/.zsh-sdkman/zsh-sdkman.plugin.zsh
+```
+
+### Usage help
+
+Sdkman can be used as usual:
+
+```bash
+Usage: sdk <command> [candidate] [version]
+       sdk offline <enable|disable>
+
+   commands:
+       install   or i    <candidate> [version] [local-path]
+       uninstall or rm   <candidate> <version>
+       list      or ls   [candidate]
+       use       or u    <candidate> <version>
+       default   or d    <candidate> [version]
+       home      or h    <candidate> <version>
+       env       or e    [init]
+       current   or c    [candidate]
+       upgrade   or ug   [candidate]
+       version   or v
+       broadcast or b
+       help
+       offline           [enable|disable]
+       selfupdate        [force]
+       update
+       flush             <broadcast|archives|temp>
+
+   candidate  :  the SDK to install: groovy, scala, grails, gradle, kotlin, etc.
+                 use list command for comprehensive list of candidates
+                 eg: $ sdk list
+   version    :  where optional, defaults to latest stable if not provided
+                 eg: $ sdk install groovy
+   local-path :  optional path to an existing local installation
+                 eg: $ sdk install groovy 2.4.13-local /opt/groovy-2.4.13
+```
+
+Here is the list of aliases available through this plugin:
+
+| Alias         | Command     |
+| ------------- |-------------|
+| sdki | sdk install    |
+| sdkun | sdk uninstall |
+| sdkls | sdk list  |
+| sdku | sdk use  |
+| sdkd | sdk default  |
+| sdkc | sdk current  |
+| sdkup | sdk upgrade  |
+| sdkv | sdk version  |
+| sdkb | sdk broadcast  |
+| sdko | sdk offline  |
+| sdksu | sdk selfupdate|  
+| sdkf | sdk flush  |
+
+## Updating sdkman
+
+The plugin comes with a zsh function to update [sdkman](https://github.com/sdkman) manually
+
+```shell
+# From zsh shell
+update_zsh_sdkman
+```
+
+## License
+
+[MIT](LICENCE)
---
+++ zsh-sdkman.plugin.zsh
@@ -0,0 +1,134 @@
+#!/usr/bin/env zsh
+
+#####################
+# COMMONS
+#####################
+autoload colors
+
+#########################
+# CONSTANT
+#########################
+
+BOLD="bold"
+NONE="none"
+
+#########################
+# PLUGIN MAIN
+#########################
+
+[[ -z "$SDKMAN_DIR" ]] && export SDKMAN_DIR="$HOME/.sdkman"
+ZSH_SDKMAN_VERSION_FILE=${SDKMAN_DIR}/version.txt
+
+#########################
+# Functions
+#########################
+
+_zsh_sdkman_log() {
+  local font=$1
+  local color=$2
+  local msg=$3
+
+  if [ $font = $BOLD ]
+  then
+    echo $fg_bold[$color] "[zsh-sdkman-plugin] $msg" $reset_color
+  else
+    echo $fg[$color] "[zsh-sdkman-plugin] $msg" $reset_color
+  fi
+}
+
+_zsh_sdkman_check_requirement() {
+    local command=$1
+    _zsh_sdkman_log $NONE "blue" "#############################################"
+    _zsh_sdkman_log $NONE "blue" "Looking for ${command}..."
+    which "${command}" > /dev/null 2>&1
+    if [[ $? -eq 1 ]]; then
+        _zsh_sdkman_log $NONE "blue" "Not found"
+        _zsh_sdkman_log $NONE "blue" "========================================="
+        _zsh_sdkman_log $NONE "blue" "Please install ${command} on your system"
+        _zsh_sdkman_log $NONE "blue" "using your favourite package manager."
+        _zsh_sdkman_log $NONE "blue" "========================================="
+        exit 1
+    fi
+
+}
+
+_zsh_sdkman_check_requirement_list() {
+  _zsh_sdkman_check_requirement zip
+  _zsh_sdkman_check_requirement unzip
+  _zsh_sdkman_check_requirement curl
+}
+
+
+_zsh_sdkman_last_version() {
+  echo $(curl -s https://get.sdkman.io/ | grep SDKMAN_VERSION | head -n 1 | cut -d '"' -f 2)
+}
+
+_zsh_sdkman_install() {
+  _zsh_sdkman_log $NONE "blue" "#############################################"
+  _zsh_sdkman_log $BOLD "blue" "Installing sdkman..." 
+  _zsh_sdkman_check_requirement_list
+  local last_version=$(_zsh_sdkman_last_version)
+  _zsh_sdkman_log $NONE "blue" "-> retrieve last version of sdkman..."
+  _zsh_sdkman_log $NONE "blue" "  -> download and install sdkman ${last_version}"
+  curl -s "https://get.sdkman.io" | bash > /dev/null 2>&1
+  echo ${last_version} > ${ZSH_SDKMAN_VERSION_FILE}
+  _zsh_sdkman_log $BOLD "green" "Install OK"
+  _zsh_sdkman_log $NONE "blue" "#############################################"
+}
+
+
+update_zsh_sdkman() {
+  _zsh_sdkman_log $NONE "blue" "#############################################"
+  _zsh_sdkman_log $BOLD "blue" "Checking new version of sdkman..."
+  
+  local current_version=$(cat ${ZSH_SDKMAN_VERSION_FILE})
+  local last_version=$(_zsh_sdkman_last_version)
+
+  if is-at-least ${last_version//+/.} ${current_version}
+  then
+    _zsh_sdkman_log $BOLD "green" "Already up to date, current version : ${current_version}"
+  else
+    _zsh_sdkman_log $NONE "blue" "-> Updating sdkman..." 
+     sdk selfupdate force > /dev/null 2>&1
+     echo ${version} > ${ZSH_SDKMAN_VERSION_FILE}
+    _zsh_sdkman_log $BOLD "green" "Update OK"
+  fi
+  _zsh_sdkman_log $NONE "blue" "#############################################"
+}
+
+_zsh_sdkman_load() {
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    # # export PATH
+    # export PATH=${PATH}:${SDKMAN_DIR}
+}
+
+
+# install sdkman if it isnt already installed
+[[ ! -f "${ZSH_SDKMAN_VERSION_FILE}" ]] &&_zsh_sdkman_install
+
+# load sdkman if it is installed
+if [[ -f "${ZSH_SDKMAN_VERSION_FILE}" ]]; then
+    _zsh_sdkman_load
+    # Rebuild completion
+    0=${(%):-%N}
+    fpath=(${0:A:h} $fpath)
+    autoload -U compinit && compinit -C
+fi
+
+########################################################
+##### ALIASES
+########################################################
+alias sdki='sdk install'
+alias sdkun='sdk uninstall'
+alias sdkls='sdk list'
+alias sdku='sdk use'
+alias sdkd='sdk default'
+alias sdkc='sdk current'
+alias sdkup='sdk upgrade'
+alias sdkv='sdk version'
+alias sdkb='sdk broadcast'
+alias sdko='sdk offline'
+alias sdksu='sdk selfupdate'
+alias sdkf='sdk flush'
+
+unset -f _zsh_sdkman_install _zsh_sdkman_check_requirement_list _zsh_sdkman_check_requirement _zsh_sdkman_load
